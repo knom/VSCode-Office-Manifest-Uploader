687c58ca6ca66ccb6c1f304eadda23eadb7510f6
Max Knor <mknor@microsoft.com>
2016-01-26 08:58:05 -0800

    Added output channel logging

--- src/installApplication.ts
+++ src/installApplication.ts
@@ -5,7 +5,11 @@ import * as ews from "./lib/ews-soap/exchangeClient";
 import * as xml2js from "xml2js";
 
 export abstract class OfficeApplicationStrategy {
+    protected outChannel: vscode.OutputChannel;
+
     constructor(public description: string) {
+        this.outChannel = vscode.window.createOutputChannel("Office Manifest Uploader");
+        this.outChannel.clear();
     }
 
     protected showUserBox(): Thenable<string> {
@@ -59,25 +63,35 @@ export abstract class OfficeApplicationStrategy {
     }
 
     public execute() {
+        this.outChannel.show();
+
         if (!vscode.workspace.rootPath) {
+            this.outChannel.appendLine("No workspace opened!");
             vscode.window.showErrorMessage("Open a workspace first!");
             return;
         }
 
         vscode.window.showInputBox({ placeHolder: "manifest.xml", prompt: "Manifest file path", value: "manifest.xml" }).then((filename) => {
-            if (filename === undefined)
+            if (filename === undefined) {
+                this.outChannel.appendLine("No file name entered!");
                 return;
+            }
             else {
                 let path = require("path");
                 let f = path.join(vscode.workspace.rootPath, filename);
 
+                this.outChannel.appendLine("Opening file " + f);
+
                 vscode.workspace.openTextDocument(f).then((file) => {
+                    this.outChannel.appendLine("Successfully opened file " + f);
+
                     let truePath = file.fileName;
 
                     this.showUserBox().then((user) => {
                         this.showPwdBox().then((pwd) => {
                             this.showServerBox().then((server) => {
-                                vscode.window.showInformationMessage("Starting " + this.description + "...");
+                                // vscode.window.showInformationMessage("Starting " + this.description + "...");
+                                this.outChannel.appendLine("Starting " + this.description + " to " + server + " as " + user);
 
                                 let statusBarItem = vscode.window.createStatusBarItem();
 
@@ -88,9 +102,11 @@ export abstract class OfficeApplicationStrategy {
                                 this.executeCore(truePath, user, pwd, server).then(() => {
                                     statusBarItem.hide();
                                     vscode.window.showInformationMessage("Succeeded " + this.description + "!");
+                                    this.outChannel.appendLine("Succeeded " + this.description + "!");
                                 }, (err) => {
                                     statusBarItem.hide();
                                     vscode.window.showErrorMessage("An error occurred while " + this.description + ": " + err);
+                                    this.outChannel.appendLine("An error occurred while " + this.description + ": " + err);
                                 });
                             });
                         });
@@ -116,6 +132,10 @@ export class InstallApplication extends OfficeApplicationStrategy {
         let promise = q.defer<void>();
 
         let data = fs.readFileSync(filePath).toString();
+
+        // this.outChannel.appendLine("Installing application manifest:");
+        // this.outChannel.append(data);
+
         let manifest = new Buffer(data).toString("base64");
 
         let client = new ews.EWSClient();
@@ -146,6 +166,8 @@ export class UninstallApplication extends OfficeApplicationStrategy {
         let manifestXml = fs.readFileSync(filePath).toString();
 
         this.getApplicationIdOutofXml(manifestXml).then((appId) => {
+            this.outChannel.appendLine("Uninstalling application with id " + appId);
+
             let client = new ews.EWSClient();
             client.initialize({ url: serverUrl, username: userName, password: password },
                 (err: any) => {
